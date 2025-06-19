cdb93064ac4387f1cfa90da9708594f3b5c1b95b
Julian Krede <julian.krede@mi.unc.edu.ar>
2025-06-19 14:25:41 -0300

    correcciones de software

--- build/default/debug/prueba_modulo_display.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/prueba_modulo_display.o:  \
-	C:/Users/Julian/MPLABXProjects/Trabajo\ Practico\ Final.X/prueba_modulo_display.asm  \
-
--- build/default/production/prueba_modulo_display.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/prueba_modulo_display.o:  \
-	C:/Users/Julian/MPLABXProjects/Trabajo\ Practico\ Final.X/prueba_modulo_display.asm  \
-
